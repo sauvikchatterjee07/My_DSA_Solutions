8dacb1e0b4a1e86e95cad0b9284c5a8f13b7e5e7
Sauvik Chatterjee <73000146+sauvikchatterjee07@users.noreply.github.com>
2022-01-26 22:56:24 +0530

    Create 1290. Convert Binary Number in a Linked List to Integer.cpp

---
+++ 1290. Convert Binary Number in a Linked List to Integer.cpp
@@ -0,0 +1,9 @@
+    int getDecimalValue(ListNode* head) {
+        int sum = 0;
+        while(head!=NULL){
+            sum *= 2;
+            sum += head->val;
+            head = head->next;
+        }
+        return sum;
+    }
