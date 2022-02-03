205c0ab552209a312fd6d1a78ce34cd55d4d79a5
Sauvik Chatterjee <73000146+sauvikchatterjee07@users.noreply.github.com>
2022-02-03 19:21:16 +0530

    Create 1512. Number of Good Pairs.cpp

---
+++ 1512. Number of Good Pairs.cpp
@@ -0,0 +1,8 @@
+    int numIdenticalPairs(vector<int>& nums) {
+        int res = 0;
+        unordered_map<int, int> count;
+        for (int a: nums) {
+            res += count[a]++;
+        }
+        return res;
+    }
