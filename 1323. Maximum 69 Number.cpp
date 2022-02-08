41f324939b902cc313b3737ae015ec1a0dcf728f
Sauvik Chatterjee <73000146+sauvikchatterjee07@users.noreply.github.com>
2022-02-08 19:59:42 +0530

    Create 1323. Maximum 69 Number.cpp

---
+++ 1323. Maximum 69 Number.cpp
@@ -0,0 +1,10 @@
+    int maximum69Number (int num) {
+        string snum = to_string(num);
+        for(auto &c : snum){
+            if(c == '6'){
+                c = '9';
+                break;
+            }
+        }
+        return stoi(snum);
+    }
