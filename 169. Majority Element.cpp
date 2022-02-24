6d3acab4eed910bdb16d510930097a66f4c8ccac
Sauvik Chatterjee <73000146+sauvikchatterjee07@users.noreply.github.com>
2022-02-24 20:34:24 +0530

    Create 169. Majority Element.cpp

---
+++ 169. Majority Element.cpp
@@ -0,0 +1,24 @@
+   
+   
+   #Moore's Voting Algorithm
+   
+   
+   
+   int majorityElement(vector<int>& nums) {
+        
+        int val = nums[0];
+        int cnt = 1;
+        
+        for(int i = 1;i<nums.size(); i++){
+            if(val == nums[i])
+                cnt++;
+            else
+                cnt--;
+            
+            if(cnt == 0){
+                val = nums[i];
+                cnt = 1;
+            }
+        }
+        return val;
+    }
